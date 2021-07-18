5a32d4b4a55050471958e4e2cdff879c5bad35f1
realrgt <ergitorubildo@gmail.com>
2021-07-18 14:29:37 +0200

    input conversion

---
+++ lib/app/core/util/input_converter.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd_clean_arch/app/core/errors/failure.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str) {
+    try {
+      final integer = int.parse(str);
+      if (integer < 0) throw FormatException();
+      return Right(integer);
+    } on FormatException {
+      return Left(InvalidInputFailure());
+    }
+  }
+}
+
+class InvalidInputFailure extends Failure {}
